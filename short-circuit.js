7897dd4c806a9435fbacdc9e44c576da1cfb7b50
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-06-11 15:50:35 +0700

    Short circuit and logical operator update

--- short-circuit.js
+++ short-circuit.js
@@ -8,7 +8,9 @@ console.log(true || false);
 
 console.log(true && false);
 
-// ??
+// ?? --> nullish coalescing
 
 console.log(true ?? false);
 console.log(false ?? "Some string");
+console.log(null ?? "Some string");
+console.log(0 ?? "Some string");
